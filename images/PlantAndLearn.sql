3b8276f93aa6d015ebeec7d7f8711dc33e3a66dd
sponomary <alex.ponomaryova@gmail.com>
2022-05-29 18:39:14 +0200

    Minor fixes

---
+++ images/PlantAndLearn.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : dim. 29 mai 2022 à 15:59
+-- Version du serveur : 5.7.34
+-- Version de PHP : 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `PlantAndLearn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Families`
+--
+
+CREATE TABLE `Families` (
+  `family_id` int(11) NOT NULL,
+  `family_name` varchar(100) NOT NULL,
+  `need_water` varchar(100) DEFAULT NULL,
+  `need_light` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `Families`
+--
+
+INSERT INTO `Families` (`family_id`, `family_name`, `need_water`, `need_light`) VALUES
+(1, 'Araceae', NULL, NULL),
+(2, 'Marantaceae', NULL, NULL),
+(3, 'Asparagaceae', NULL, NULL),
+(4, 'Arecaceae', NULL, NULL),
+(5, 'Begoniaceae', NULL, NULL),
+(6, 'Urticaceae', NULL, NULL),
+(7, 'Asclepiadaceae', NULL, NULL),
+(8, 'Commelinaceae', NULL, NULL),
+(9, 'Piperaceae', NULL, NULL),
+(10, 'Acanthaceae', NULL, NULL),
+(11, 'Mimosaceae', NULL, NULL),
+(12, 'Asclepiadaceae', NULL, NULL),
+(13, 'Gesneriaceae', NULL, NULL),
+(14, 'Orchidaceae', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Plants`
+--
+
+CREATE TABLE `Plants` (
+  `plant_id` int(11) NOT NULL,
+  `scientific_name` varchar(100) DEFAULT NULL,
+  `family` int(11) DEFAULT NULL,
+  `common_name_en` varchar(150) DEFAULT NULL,
+  `common_name_fr` varchar(150) DEFAULT NULL,
+  `common_name_ru` varchar(150) DEFAULT NULL,
+  `photo` varchar(2000) DEFAULT NULL,
+  `notes` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `Plants`
+--
+
+INSERT INTO `Plants` (`plant_id`, `scientific_name`, `family`, `common_name_en`, `common_name_fr`, `common_name_ru`, `photo`, `notes`) VALUES
+(1, 'Philodendron hastatum', 1, 'Silver sword, silver queen', 'Épée d\'argent', 'Филодендрон копьевидный', 'Philodendron_hastatum.avif', NULL),
+(2, 'Alocasia reginula', 1, 'Alocasia little queen', 'Oreille d’éléphant, Joyau de la reine', 'Алоказия реджинула', 'Alocasia_reginula.avif', NULL),
+(3, 'Monstera adansonii', 1, 'Monkey mask, Swiss cheese plant', 'Masque de singe, Plante gruyère', 'Обезьянья маска филодендрон, Дырчатое растение ', 'Monstera_adansonii.avif', NULL),
+(4, 'Aglaonema', 1, 'Chinese evergreen', 'Aglaonème', 'Аглаонема', 'Aglaonema.avif', NULL),
+(5, 'Maranta leuconeura fascinator', 2, 'Maranta tricolour, Prayer plant', 'Plante prieuse, plante dormeuse', 'Молящаяся трава', 'Maranta_leuconeura_fascinator.avif', NULL),
+(6, 'Cordyline fruticosa', 3, 'Cabbage palm', 'Épinard hawaïen', 'Кордилина кустарниковая', 'Cordyline_fruticosa.avif', NULL),
+(7, 'Howea forsteriana', 4, 'Kentia palm', 'Kentia de Foster', 'Ховея Форстера', 'Howea_forsteriana.jpeg', NULL),
+(8, 'Syngonium podophyllum', 1, 'Arrowhead plant, goosefoot, nephthytis', 'Patte-d\'oie', 'Сингониум ножколистный', 'Syngonium_podophyllum.avif', NULL),
+(9, 'Goeppertia makoyana', 2, 'Cathedral plant, peacock plant', 'Plante de fenêtres cathédrale, plante de paon', 'Калатея Макоя, павлиний цветок', 'Goeppertia_makoyana.avif', NULL),
+(10, 'Begonia maculata', 5, 'Trout begonia, Polka dot begonia, Spotted begonia', 'Tamaya, bégonia bambou', 'Бегония макулата', 'Begonia_maculata.jpeg', NULL),
+(11, 'Pilea peperomioides', 6, 'Chinese money plant, UFO plant, pancake plant, missionary plant', 'Plante à monnaie chinoise, lefse plant, plante du missionnaire', 'Пилея Кадье, китайское денежное дерево', 'Pilea_peperomioides.avif', NULL),
+(12, 'Hoya', 7, 'Wax plant, porcelain flower', 'Fleurs de cire, fleurs de porcelaine', 'Восковой плющ', 'Hoya.avif', NULL),
+(13, 'Tradescantia zebrina', 8, 'Silver inch plant, wandering Jew', 'Misère', 'Традесканция висячая, блуждающий еврей', 'Tradescantia_zebrina.jpeg', NULL),
+(14, 'Anthurium crystallinum', 1, 'Crystal Anthurium', 'Anthurium crystal', 'Антуриум хрустальный', 'Anthurium_crystallinum.avif', NULL),
+(15, 'Peperomia caperata', 9, 'Emerald ripple peperomia', 'Canne d\'aveugle', 'Пеперомия сморщенная', 'Peperomia_caperata.jpeg', NULL),
+(17, 'test', 6, 'test', 'test', 'test', '6293973bca73c5.44475311.', 'test'),
+(18, 'ete', 10, 'rete', 'ret', 'erte', '629397a34e3e54.90410054.', 'ert');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Users`
+--
+
+CREATE TABLE `Users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Plants`
+--
+ALTER TABLE `Plants`
+  ADD PRIMARY KEY (`plant_id`);
+
+--
+-- Index pour la table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Plants`
+--
+ALTER TABLE `Plants`
+  MODIFY `plant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT pour la table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- quiz.php
+++ quiz.php
@@ -7,81 +7,96 @@
     <section class="colored-section" id="bao1">
 
         <?php
+
+        
           require_once("config.php");
           require_once("functions.php");
 
           entete("Quiz", "utf-8", "css/styles.css");
           debut("Quiz");
 
-          echo "VERSION 4";
+          echo "VERSION 12";
           echo "<br />";
 
+        //if ($_GET["start_game"] == "go") {
           # Vérifier le nombre d'entrées dans le tableau
           $requete = $sql->prepare("SELECT COUNT(*) AS max_num FROM Plants");
           $requete->execute();
           $ligne = $requete->fetch();
           $max_num = $ligne['max_num'];
-          echo $max_num;
-          echo "<br />";
 
           # Utiliser le nombre maximal d'entrées pour générer le nombre aléatoire
           $hasard = rand(0, $max_num);
-          echo $hasard;
+       
           echo "<br />";
 
-          if ($_GET["start_game"] == "go") {
+          //
 
             // Saisir un nom commun en français au hasard parmi ceux existant dans la BDD
             $requete = $sql->prepare("SELECT common_name_fr FROM Plants WHERE plant_id = :param_id");
             $requete->execute(['param_id' => $hasard]);
             $source = $requete->fetch();
 
-            echo $source['common_name_fr']; // terme dont il faut trouver l'equivalent
-
             echo "<legend>Q : Trouvez l'equivalent du terme ".$source['common_name_fr']." en anglais : </legend>";
-        }  
-        else {
-          echo "Tant pis pour vous.";
-        }
+
+            $requete = $sql->prepare("SELECT `plant_id`, `common_name_en` FROM `Plants` WHERE `plant_id` = :param_id UNION ALL
+            (SELECT `plant_id`, `common_name_en` FROM `Plants` WHERE `plant_id` != :param_id ORDER BY RAND() limit 5) ORDER BY RAND()");
+            $requete->execute(['param_id' => $hasard]);
+            $lignes = $requete->fetchAll();
+
+            ?>
+
+            <iframe id="myIframe" name="myIframe" width="0" height="0" frameborder="0"></iframe>
+            <form action="quiz.php" target="myIframe" method="POST">
+              <div class="col">
+                <label for="options" class="form-label"> Faites le choix : </label><br />
+
+            <?php
+            foreach ($lignes as $ligne) {
+              echo "<input id='option' type='radio' class='form-check-input' name='reponse' value='".$ligne['plant_id']."'>".$ligne['common_name_en']."<br />";
+            }
+
+            ?>
+              <div class="col-md-12">
+                <button type="submit" name="confirmer" class="btn btn-success">Confirmer</button>
+              </div>
+            </div>
+          </form>
+
+
+          <?php   
+
+            if(isset($_POST['confirmer']))
+            {
+              // Récuperer la réponse
+              $reponse = $_POST["reponse"];
+              echo "<br/>";
+              echo $hasard;
+              echo "<br/>";
+              echo $reponse;
+              echo "<br/>";
+
+              // Verifier si la réponse donnée est correcte
+              if ($hasard == $reponse ) {
+                echo "<p>Correct</p>";
+                echo '<button type=" submit" name="ok" value="confirmer" class="btn btn-success">Continuer</button>';
+              }
+              else {
+                echo "<p>Faux</p>";
+              }
+
+            }
+  
+        //}  
+        //else {
+          //echo "Tant pis pour vous.";
+       // }
 
       ?>
 
-        <form action="">
-            <div class="col">
-                <label for="options" class="form-label"> Faites le choix : </label><br />
-                <?php
-          
-          for ($i=0; $i<3; $i++) {
+       
 
-            $requete = $sql->prepare("SELECT DISTINCT common_name_en FROM Plants WHERE plant_id = ROUND( RAND() * 9 ) + 1");
-            $var = $requete->execute();
-            echo $var;
-            
-            echo '<input id="option" type="radio" class="form-check-input" name="reponse" value="" />'.$var."<br />";
-          }
-         ?>
-                <div class="col-md-12">
-                    <button type=" submit" name="go" value="start_game" class="btn btn-success">Confirmer</button>
-                </div>
-            </div>
-        </form>
-
-        <?php   
-          // Récuperer la réponse
-          $reponse = $_GET["confirmer"];
-
-          // Verifier si la réponse donnée est correcte
-          if ($source == $reponse ) {
-            echo "<p>Correct</p>";
-            echo '<button type=" submit" name="ok" value="confirmer" class="btn btn-success">Continuer</button>';
-          }
-          else {
-            echo "<p>Faux</p>";
-          }
-
-          retour("index.php");
-          
-        ?>
+        
 
         </div>
     </section>
